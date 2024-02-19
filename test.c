4a47aacf69d4773d424b6d13d24ba304ae0d8b2c
William <willspri@gmail.com>
2024-02-20 12:14:25 +1300

    commit

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  //variables
+  int myNum = 15;              // Integer
+  float myFloatNum = 5.99;     // Floating point
+  char myLetter = 'F';         // Character
+
+  // Print variables
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  return 0;
+}
